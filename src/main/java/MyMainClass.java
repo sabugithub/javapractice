baaed24486494247dbcbebc14f539b43d1466141
Sabu <sabu.thajudeen@gmail.com>
2016-02-13 10:14:23 +0000

    My second commit

--- src/main/java/MyMainClass.java
+++ src/main/java/MyMainClass.java
@@ -3,6 +3,11 @@ public class MyMainClass {
 
  public static void main(String args[]){
      System.out.println("Sabu Thajudeen");
+
+     for(int i=0;i<10;i++){
+
+         System.out.println(i);
+     }
  }
 }
 
